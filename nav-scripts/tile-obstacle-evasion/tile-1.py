64e2ad8ebbf5c73078b4c14a0358c43cad8c74b8
mbrak0 <mbrako12@gmail.com>
2021-11-21 17:10:46 +0000

    Completed basic obstacle evasion script - obstacles detected by white(-ish) pixels and camera looks right when obstacle is encountered

--- nav-scripts/tile-obstacle-evasion/tile-1.py
+++ nav-scripts/tile-obstacle-evasion/tile-1.py
@@ -7,23 +7,6 @@ import time
 import glob
 import os
 
-#file_count = 0
-
-#sys.stdout.flush()
-
-#while(1):
-
-#print("w")
-#print("c")
-
-"""
-#list_of_files = glob.glob('/home/matt-ip/Desktop/ForestGenerator-1.2/frames/*')
-#print("c")
-#latest_file = max(list_of_files, key=os.path.getctime)
-#print("c")
-#print(latest_file)
-"""
-
 """
 file_count = 0
 
@@ -34,13 +17,11 @@ for path in pathlib.Path("/home/matt-ip/Desktop/ForestGenerator-1.2/frames").ite
 #print(file_count)
 
 print("c")
-
 """
 
 #imagename = "/home/matt-ip/Desktop/ForestGenerator-1.2/frames/frame--depth-" + str(file_count-1) + ".jpg"
 imagename = "/home/matt-ip/Desktop/ForestGenerator-1.2/frames/frame--depth-0.jpg"
 #print(imagename)
-#img = cv2.imread(latest_file, 0) # 0 params, for grey image
 img = cv2.imread(imagename, 0) # 0 params, for grey image; 600x800 image
 rows, cols = img.shape[:2]  # image height and width
 #print(img)  # all image pixels value in array
@@ -79,14 +60,11 @@ for y in range(0,rows,M):
 
 cv2.imwrite("/home/matt-ip/Desktop/auto-forest-nav/img-tiles/img-tiles.jpg", img)
 
-		
-#print("c")
 if obstacle == False:
 	print("w")
 else:
 	print("l")
 
-#sys.stdout.flush()
+sys.stdout.flush()
 
-
-#time.sleep(0.1)
+time.sleep(0.1)

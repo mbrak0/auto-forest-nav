80f647aef488143a62b009d753684084e13268df
mbrak0 <mbrako12@gmail.com>
2022-04-02 12:38:12 +0100

    Completed regions weighted average algorithm using a normal distribution; initially tested with linear and logarithmic scales for weights but did not achieve desirable performance

--- boxplots.py
+++ boxplots.py
@@ -2,76 +2,82 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def logNum(x):
-    return(x[7:9])
+seed_num = list(range(1,6))
+#print(seed_num)
 
-path1 = '/home/matt-ip/Desktop/logs/New2/Density/sparse10_90/stats/'
-list_of_files1 = os.listdir(path1)
-list_of_files1 = sorted(list_of_files1, key = logNum)
-#print(list_of_files1)
+alg_select = [1,2]
 
-num_moves_1_arr = []
-euc_dis_1_arr = []
-wobble_1_arr = []
+moves_num_arr = []
+euc_dis_arr = []
+wobble_rate_arr = []
+col_arr = []
 
-for file in list_of_files1:
-    
-    file_path1 = path1 + file
-    f1 = open(file_path1, "r")
-    stats1 = f1.readlines()
-    
-    num_moves1 = int((stats1[16].split())[4])
-    num_forward1 = int((stats1[17].split())[5])
-    num_right1 = int((stats1[18].split())[5])
-    num_left1 = int((stats1[19].split())[5])
+"""
+path1 = "/home/matt-ip/Desktop/logs/algorithm1/density/sparse/seed1/logfile.txt"
 
-    euc_dis1 = (7/30) * num_forward1
-    wobble1 = (num_right1 + num_left1) / num_moves1
-    
-    num_moves_1_arr.append(num_moves1)
-    euc_dis_1_arr.append(euc_dis1)
-    wobble_1_arr.append(wobble1)
-
+f1 = open(path1, "r")
+log1 = f1.readlines()
 f1.close()
 
-path2 = '/home/matt-ip/Desktop/logs/NewObs/Density/sparse/3regions2/stats/'
-list_of_files2 = os.listdir(path2)
-list_of_files2 = sorted(list_of_files2, key = logNum)
+num_of_moves = log1[16].split()[4]
+euc_dis = log1[21].split()[4]
+wobble_rate = log1[22].split()[3]
 
-num_moves_2_arr = []
-euc_dis_2_arr = []
-wobble_2_arr = []
+moves_num_arr.append(num_of_moves)
+euc_dis_arr.append(euc_dis)
+wobble_rate_arr.append(wobble_rate)
+"""
 
-for file in list_of_files2:
-    
-    file_path2 = path2 + file
-    f2 = open(file_path2, "r")
-    stats2 = f2.readlines()
-    
-    num_moves2 = int((stats2[16].split())[4])
-    num_forward2 = int((stats2[17].split())[5])
-    num_right2 = int((stats2[18].split())[5])
-    num_left2 = int((stats2[19].split())[5])
+for i in alg_select:
+    for j in seed_num:
+        path = "/home/matt-ip/Desktop/logs/algorithm" + str(i) + "/density/sparse/seed" + str(j) + "/logfile.txt"
+        #print(path)
 
-    euc_dis2 = (7/30) * num_forward2
-    wobble2 = (num_right2 + num_left2) / num_moves2
-    
-    num_moves_2_arr.append(num_moves2)
-    euc_dis_2_arr.append(euc_dis2)
-    wobble_2_arr.append(wobble2)
+        f = open(path, "r")
+        log = f.readlines()
+        f.close()
 
-f2.close()
+        num_of_moves = float(log[16].split()[4])
+        euc_dis = float(log[21].split()[4])
+        wobble_rate = float(log[22].split()[3])
+        num_of_collisions = float(log[23].split()[4])
 
-moves_data = [num_moves_1_arr, num_moves_2_arr]
-euc_dis_data = [euc_dis_1_arr, euc_dis_2_arr]
-wobble_data = [wobble_1_arr, wobble_2_arr]
+        moves_num_arr.append(num_of_moves)
+        euc_dis_arr.append(euc_dis)
+        wobble_rate_arr.append(wobble_rate)
+        col_arr.append(num_of_collisions)
 
+#print(moves_num_arr)
+#print(euc_dis_arr)
+#print(wobble_rate_arr)
+#print(col_arr)
+
+moves_data = [moves_num_arr[0:5], moves_num_arr[5:10]]
+euc_dis_data = [euc_dis_arr[0:5], euc_dis_arr[5:10]]
+wobble_data = [wobble_rate_arr[0:5], wobble_rate_arr[5:10]]
+collision_data = [col_arr[0:5], col_arr[5:10]]
+
+#print(moves_num_arr[0:5])
 #print(moves_data)
 
 fig = plt.figure(figsize=(10,7))
 ax = fig.add_axes([0.1,0.1,0.8,0.8])
 
-bp = ax.boxplot(moves_data)
+#bp = ax.boxplot(moves_data)
+#plt.ylabel("Number of Moves")
+
 #bp = ax.boxplot(euc_dis_data)
+#plt.ylabel("Euclidean Distance Travelled (m)")
+
 #bp = ax.boxplot(wobble_data)
+#plt.ylabel("Wobble Rate")
+
+#bp = ax.boxplot(collision_data)
+#plt.ylabel("Number of Collisions")
+
+plt.xticks([1, 2], ["Algorithm 1", "Algorithm 2"])
+
+boxplot_label = "collisions_1v2"
+fig.savefig("/home/matt-ip/Desktop/logs/Boxplots/" + str(boxplot_label))
+
 plt.show()
--- nav-scripts/algorithm2/regions-average.py
+++ nav-scripts/algorithm2/regions-average.py
@@ -81,6 +81,10 @@ move_arr = []
 
 obs_arr = []
 
+least_obs_arr = []
+reg_stuck = False
+
+
 while goal_reached(true_x_pos, true_z_pos, true_x_goal_pos, true_z_goal_pos) == False:
 
 	if moves_exceeded(move_count) == False:
@@ -191,6 +195,7 @@ while goal_reached(true_x_pos, true_z_pos, true_x_goal_pos, true_z_goal_pos) == 
 				print(border_move)				
 
 			obs_arr.append("N/A")
+			least_obs_arr.append("N/A")
 			
 			sys.stdout.flush()
 			time.sleep(0.1)
@@ -208,6 +213,7 @@ while goal_reached(true_x_pos, true_z_pos, true_x_goal_pos, true_z_goal_pos) == 
 					sys.stdout.flush()
 					time.sleep(0.1)
 					obs_arr.append("N/A")
+					least_obs_arr.append("N/A")
 				
 				elif goal_angle < 0:
 					print("l")
@@ -215,74 +221,96 @@ while goal_reached(true_x_pos, true_z_pos, true_x_goal_pos, true_z_goal_pos) == 
 					sys.stdout.flush()
 					time.sleep(0.1)
 					obs_arr.append("N/A")
+					least_obs_arr.append("N/A")
 
 			if dir_check < 20:
 
 				img = cv2.imread(latest_file, 0) # 0 params, for grey image
 				rows, cols = img.shape[:2]  # image height and width
 
-				y1 = 0
-				x1 = 0
-				M = rows//20 # image height divided by 20
-				N = cols//20 # image width divided by 20
+				left_reg = img[0:rows, 0:int(round((1/3)*cols)-1)]
+				right_reg = img[0:rows, int(round((2/3)*cols)+1):cols]
+				mid_reg = img[0:rows, int(round((1/3)*cols)-1):int(round((2/3)*cols)+1)]
+				
+				left_mean, right_mean, mid_mean = np.mean(left_reg), np.mean(right_reg), np.mean(mid_reg)
 
-				obstacle = False
+				reg_mean_arr = [left_mean, mid_mean, right_mean]
 
-				obs_left = 0
-				obs_right = 0
+				least_obs = np.argmax(reg_mean_arr)
+				least_obs_arr.append(least_obs)
 
-				for y in range(0,int(0.6*rows),M):
-					for x in range(0,cols,N):
-						y1 = y + M
-						x1 = x + N
-						tile = img[y:y+M, x:x+N]
+				most_obs = np.argmin(reg_mean_arr)
 
-						#thresh = cv2.threshold(tile, 31.875, 255, cv2.THRESH_BINARY_INV)[1]
-						thresh = cv2.threshold(tile, 25.5, 255, cv2.THRESH_BINARY_INV)[1]
-						pixels = cv2.countNonZero(thresh)
-
-						if pixels == (M * N):
-							obstacle = True
-							if x < 400:
-								obs_left +=1
-							else:
-								obs_right +=1
-
-				if obstacle == False:
-					obs_arr.append("False")
-					print("w")
-					move_arr.append("w_gen")
-					dir_check += 1
-
-				elif obstacle == True:
+				if reg_mean_arr[most_obs] <= 40:
 
 					obs_arr.append("True")
 
-					if obs_left > obs_right:
-						if (move_arr[move_count-1] == "j_obs"):
+					left_half = img[0:rows, 0:int(0.5*cols)]
+					right_half = img[0:rows, int(0.5*cols):cols]
+
+					left_half_mean, right_half_mean = np.mean(left_half), np.mean(right_half)
+
+					if left_half_mean < right_half_mean:
+						if (move_arr[move_count-1] == "j_obs") or (move_arr[move_count-1] == "j_reg") or (move_arr[move_count-1] == "j_reg_stuck"):
 							print("j")
 							move_arr.append("j_obs")
 						else:
 							print("l")
 							move_arr.append("l_obs")
 					else:
-						if (move_arr[move_count-1] == "l_obs"):
+						if (move_arr[move_count-1] == "l_obs") or (move_arr[move_count-1] == "l_reg"):
 							print("l")
 							move_arr.append("l_obs")
 						else:
 							print("j")
 							move_arr.append("j_obs")
-				
+
+				else:
+					
+					obs_arr.append("False")
+
+					if ((x_pos >= x_goal_pos-10) and (x_pos <= x_goal_pos+10) and (z_pos >= z_goal_pos-10) and (z_pos <= z_goal_pos+10)):
+						print("w")
+						move_arr.append("w_gen")
+						dir_check += 1
+					
+					else:
+
+						if (least_obs_arr[move_count-1] == 2) and (least_obs == 0):
+							reg_stuck = True
+						
+						if reg_stuck == True:
+							if least_obs != 1:
+								print("j")
+								move_arr.append("j_reg_stuck")
+							else:
+								reg_stuck = False
+
+						if reg_stuck == False:
+							if (least_obs == 1) or ((left_mean > mid_mean-5) and (left_mean < mid_mean+5)) or ((right_mean > mid_mean-5) and (right_mean < mid_mean+5)):
+							#if (least_obs == 1):
+								print("w")
+								move_arr.append("w_reg")
+								dir_check += 1
+							
+							elif least_obs == 0:
+								print("j")
+								move_arr.append("j_reg")
+							
+							elif least_obs == 2:
+								print("l")
+								move_arr.append("l_reg")
+
 				sys.stdout.flush()
 				time.sleep(0.1)
-
+		"""
 		f6 = open("/home/matt-ip/Desktop/logs/debug.txt", "a")
 		if dir_check < 20:
-			f6.write(str(move_count) + "-> " + str(move_arr[move_count]) + " : x: " + str(x_pos) + ", z: " + str(z_pos) + " -> obstacle = " + str(obstacle) + ", obs_left = " + str(obs_left) + ", obs_right = " + str(obs_right) + "\n")
+			f6.write(str(move_count) + "-> " + str(move_arr[move_count]) + " : x: " + str(x_pos) + ", z: " + str(z_pos) + " -> reg_stuck = " + str(reg_stuck) + ", Left mean = " + str(left_mean) + ", Mid mean = " + str(mid_mean) + ", Right mean = " + str(right_mean) + "\n")
 		else:
 			f6.write(str(move_count) + ": GOAL CORRECTION ACTIVE\n")
 		f6.close()
-
+		"""
 		move_count += 1
 		time.sleep(0.2)
 	
@@ -340,14 +368,20 @@ j_border_count = move_arr.count("j_bor")
 l_fixdir_count = move_arr.count("l_fixdir")
 j_fixdir_count = move_arr.count("j_fixdir")
 
+w_reg_count = move_arr.count("w_reg")
+l_reg_count = move_arr.count("l_reg")
+j_reg_count = move_arr.count("j_reg")
+
 w_gen_count = move_arr.count("w_gen")
 
 l_obs_count = move_arr.count("l_obs")
 j_obs_count = move_arr.count("j_obs")
 
-w_count = w_border_count + w_gen_count
-l_count = l_border_count + l_fixdir_count + l_obs_count
-j_count = j_border_count + j_fixdir_count + j_obs_count
+j_reg_stuck_count = move_arr.count("j_reg_stuck")
+
+w_count = w_border_count + w_reg_count + w_gen_count
+l_count = l_border_count + l_fixdir_count + l_reg_count + l_obs_count
+j_count = j_border_count + j_fixdir_count + j_reg_count + j_reg_stuck_count + j_obs_count
 
 euc_dis = (7/30) * w_count
 wobble_rate = (l_count + j_count) / move_count
